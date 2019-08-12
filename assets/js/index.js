5486afa0043e3c3ece83b0a0f8bb283221c0c11f
thebyteman <thebyteman256@gmail.com>
2019-08-12 09:49:45 -0400

    add comments to js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+/* on hover functionality for follow and patreon CTA */
 let followImg = document.getElementById("follow-img")
 let patreonImg = document.getElementById("patreon-img")
 
@@ -17,30 +18,35 @@ patreonImg.onmouseout = () => {
     patreonImg.setAttribute("src", "/assets/images/patreon-hide.png")
 };
 
+
+/* code to populate merch from etsy */
 const Http = new XMLHttpRequest();
 const url = 'http://thebyteman.com/api/monsterwaifusmerch';
 Http.open("GET", url);
 Http.send();
 
 Http.onreadystatechange = (e) => {
+    /* 4 depicts that the http request is fully complete and all data is present */
     if (Http.readyState == 4) {
         let merch = JSON.parse(Http.responseText)
         buildMerchCard(merch);
     }
-
 }
-
+/* copy pasta function to get a random number in a range */
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
-
+/* called in the Http request, and passed the products json object to create our merch cards */
 let buildMerchCard = (merch) => {
-
+    /* max is used for determining if we need truncate the procuts list or not */
     let max = merch.title.length;
+    /* default random numbers for if we have less than 3 products, if more, then this array gets over written */
     let randNumbers = [0, 1, 2];
+    /* set iterate to the number of products, if it's > 3 then it'll get set to 3 below */
     let iterate = max;
+    /* loop to determine which 3 random products gets listed if the amount of products is > 3 */
     if(max > 3) {
         iterate = 3;
         for (let i = 0; i < 3; i++) {
@@ -51,17 +57,23 @@ let buildMerchCard = (merch) => {
             randNumbers[i] = randNum;
         }
     }
-
+    /* get the node for the parent container to put all the merch cards into */
     let merchCardContainer = document.getElementById("merch-card-container");
 
+    /* loop to build each merch card */
     for (let i = 0; i < iterate; i++) {
         let merchCard = document.createElement('div');
+
+        let image = document.createElement('img');
+
+        let link = document.createElement('a');
+
         let title = document.createElement('h2');
         let titleText = document.createTextNode(merch.title[randNumbers[i]]);
-        let image = document.createElement('img');
-        let link = document.createElement('a');
+
         let price = document.createElement('p');
         let priceText = document.createTextNode("$ " + merch.price[randNumbers[i]]);
+
 
         link.setAttribute('href', merch.link[randNumbers[i]]);
 
